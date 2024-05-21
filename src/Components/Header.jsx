cf8760907571cb1c8733f1bfdbb34ec32b637b09
deepakyadav121999 <dy44444221@gmail.com>
2024-05-21 08:43:51 +0530

    ma

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import logo from './motionarteffect-img2.png'
+import '../App.css'
 const Header = () => {
   return (
-    <div className='bg-gray-700 flex text-white h-15 p-5 sm:justify-between  justify-center flex-wrap items-center gap-3 fixed top-0 w-full '>
-        <div className='flex  justify-center text-center items-center gap-1'>
+    <div className='clrheader flex text-white h-15 p-3 sm:justify-between  justify-center flex-wrap items-center gap-3 fixed top-0 w-full '>
+        <div className='flex  justify-center text-center items-center gap-0'>
          <img src={logo}  className='h-9'/>
-        <a  href="" className='text-2xl'>envato <span className='text-green-500 font-sans '>market</span></a>
+        <a  href="" className='text-xl'>envato <span className='text-green-600 font-sans '>market</span></a>
         </div>
         <div>
             <div>
